b6593c277bd0b54ac1f64fb1524dd800bf9518f3
sarthakzzzzz <sarthakpujari1970@gmail.com>
2025-08-31 03:27:17 +0530

    deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { isTokenExpired } from "../utils/jwtUtils";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
